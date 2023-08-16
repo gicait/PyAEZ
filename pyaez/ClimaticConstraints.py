1d119d9683759aa0fc8b1098940aacb2cf468885
Swun Wunna Htet <72969600+NokuzaSezo97@users.noreply.github.com>
2023-08-16 09:50:54 +0700

    Update ClimaticConstraints.py
    1. Revised the calculation routine of Climatic Constraints.
    2. Added linear interpolation for locations of annual mean temperatures between 10 and 20 Degree Celsius.
    3. Removed salt-and-pepper value missing effects.

--- pyaez/ClimaticConstraints.py
+++ pyaez/ClimaticConstraints.py
@@ -8,6 +8,9 @@ PyAEZ version 2.1.0 (June 2023)
     irrigated conditions.
     The algorithm will check the annual mean temperature and assess the value from 
     the respective look-up fc3 factor to apply to yield.
+2. Added missing logic of linear interpolation for pixels with annual mean temperature between
+    10 and 20 deg Celsius to extract fc3 constraint factor.
+3. Fixing long code to short and consistent.
     
 """
 import numpy as np
@@ -58,8 +61,8 @@ class ClimaticConstraints(object):
         self.fc3_rain_lt10 = np.prod(self.fc3_reduction_fct_rain_lt_10, axis = 0)
 
 
-    
-    # New Routine
+
+    # Updated Routine
     def applyClimaticConstraints(self, min_temp, max_temp, lgp, lgpt10,  lgp_equv, yield_input, irr_or_rain, tclimate, no_tclimate):
         """
         Parameters
@@ -97,7 +100,7 @@ class ClimaticConstraints(object):
         self.LGP_agc = np.zeros(lgp.shape, dtype = int)
         self.clim_adj_yield = np.zeros(lgp.shape)
         self.fc3 = np.zeros(lgp.shape,np.float64)
-        self.lgpt10_map = lgpt10
+        self.lgpt10_map = np.copy(lgpt10)
         
         for i_row in range(self.LGP_agc.shape[0]):
             for i_col in range(self.LGP_agc.shape[1]):
@@ -112,6 +115,7 @@ class ClimaticConstraints(object):
                     
                 elif lgp[i_row, i_col] > 210:
                     self.LGP_agc[i_row, i_col] = max(210, min(lgp[i_row, i_col], lgp_equv[i_row, i_col]))
+        
         
         
         """Agro-climatic reduction factor calculaton from combination of  b, c, d and e constraints"""
@@ -133,69 +137,70 @@ class ClimaticConstraints(object):
                     
                 
                 else:
-                    
+                    # Annual mean temperature of a location
+
+                    ann_mean = self.ann_meanTdaily[i_row, i_col].copy()
+
+                    # Input for Irrigated condition
                     if irr_or_rain == 'irr':
-                            
-                        
-                        for interval in range(self.lgpt10_class_irr.shape[0]):
-                            
-                            if self.lgpt10_map[i_row, i_col] in range(self.lgpt10_class_irr[interval][0], self.lgpt10_class_irr[interval][1]):
-                                
-                                fc1_lgpt10 = self.lgpt10_irr[interval]
-                        
-                        if self.ann_meanTdaily[i_row, i_col] >= 20:
-                            
-                            for class_index in range(self.wetness_days_class_irr.shape[0]):
-                                
-                                if self.LGP_agc[i_row, i_col] in range(self.wetness_days_class_irr[class_index][0], self.wetness_days_class_irr[class_index][1]):
-                                    
-                                    fc3_irr_wtout_e = self.fc3_irr_gte20[class_index]
-                                    
-                                    self.fc3[i_row, i_col] = min(fc3_irr_wtout_e, fc1_lgpt10)
-                                
-                        elif self.ann_meanTdaily[i_row, i_col] < 10:
-                            
-                            for class_index in range(self.wetness_days_class_irr.shape[0]):
-                                
-                                if self.LGP_agc[i_row, i_col] in range(self.wetness_days_class_irr[class_index][0], self.wetness_days_class_irr[class_index][1]):
-                                    
-                                    fc3_irr_wtout_e = self.fc3_irr_lt10[class_index]
-                                    
-                                    self.fc3[i_row, i_col] =min(fc3_irr_wtout_e, fc1_lgpt10)
-                    
+
+                        lgp_class = self.wetness_days_class_irr.copy() # 13 classes
+                        lgpt10_class = self.lgpt10_class_irr.copy() # 13 classes
+                        lgpt10 = self.lgpt10_irr.copy() # 13 classes
+                        fc3_all10 = self.fc3_reduction_fct_irr_lt_10.copy()
+                        fc3_all20 = self.fc3_reduction_fct_irr_gte_20.copy()
+                 
+                    # Input for Rainfed conditions
                     elif irr_or_rain == 'rain':
+
+                        lgp_class = self.wetness_days_class_rain.copy()
+                        lgpt10_class = self.lgpt10_class_rain.copy()
+                        lgpt10 = self.lgpt10_rain.copy()
+                        fc3_all10 = self.fc3_reduction_fct_rain_lt_10.copy()
+                        fc3_all20 = self.fc3_reduction_fct_rain_gte_20.copy()
+                    
+                    else:
+                        raise Exception('Input Error: Provide either one of option (rain or irr)')
+                    
+                    # finding day-interval to extract corresponding fc3 for 'e' constraint 
+
+                    for val in range(lgpt10_class.shape[0]):
+                        startf, endf = lgpt10_class[val][0], lgpt10_class[val][1]
+                        fc3_e = 1.
                         
-                        for interval in range(self.lgpt10_class_rain.shape[0]):
-                            
-                            if self.lgpt10_map[i_row, i_col] in range(self.lgpt10_class_rain[interval][0], self.lgpt10_class_rain[interval][1]+1):
-                                
-                                fc1_lgpt10 = self.lgpt10_rain[interval]
-                                
-                        if np.isnan(self.lgpt10_map[i_row, i_col]):
-                            fc1_lgpt10 = 0.
-                        if self.ann_meanTdaily[i_row, i_col] >= 20:
-                            
-                            for class_index in range(self.wetness_days_class_rain.shape[0]):
-                                
-                                if self.LGP_agc[i_row, i_col] in range(self.wetness_days_class_rain[class_index][0], self.wetness_days_class_rain[class_index][1]+1):
-                                        
-                                    fc3_irr_wtout_e = self.fc3_rain_gte20[class_index]   
-                                    self.fc3[i_row, i_col] = np.minimum(fc3_irr_wtout_e, fc1_lgpt10)
-                                        
-                                
-                        elif self.ann_meanTdaily[i_row, i_col] < 10:
-                            
-                            for class_index in range(self.wetness_days_class_rain.shape[0]):
-                                
-                                if self.LGP_agc[i_row, i_col] in range(self.wetness_days_class_rain[class_index][0], self.wetness_days_class_rain[class_index][1]+1):
-                                        
-                                    fc3_irr_wtout_e = self.fc3_rain_lt10[class_index]   
-                                    self.fc3[i_row, i_col] = np.minimum(fc3_irr_wtout_e, fc1_lgpt10)
-                        
-                        else:
-                            self.fc3[i_row, i_col]=1                
+                        if self.lgpt10_map[i_row, i_col] in range(startf, endf+1):
+                            fc3_e = lgpt10[val]
+                            break
+                    
+                    # Check for annual mean temperature if it is on selected 20 or 10 deg.
+                    # For ann_mean >= 20 OR < 10, fc3 will be extracted from look-up table directly.
+                    # For ann_mean between 10 and 20, linear interpolation is done to day-interval specific fc3 from two look up tables.
+                    for index in range(lgp_class.shape[0]):
+                        fc3_bcd = np.zeros(3) # empty array for b,c,d constraints
+
+                        if self.LGP_agc[i_row, i_col] in range(lgp_class[index][0], lgp_class[index][1]+1):
+
+                            if ann_mean >= 20:
+                                fc3_bcd = fc3_all20[:,index]
+                                break
+
+
+                            elif ann_mean <10:
+                                fc3_bcd = fc3_all10[:,index]
+                                break
+
+                            else:
+                                # linear interpolation is done between 10 deg and 20 deg reduction factors depending on annual mean temperature
+                                for k in range(3):
+                                    fc3_bcd[k] = np.interp(ann_mean, [10,20], [fc3_all10[k, index], fc3_all20[k, index]])
+                                break
+                    
+                    # Choose the most limiting fc3 from four constraints' contribution and apply to the yield
+                    min_fc3_bcd = np.min(fc3_bcd)
+
+                    self.fc3[i_row, i_col] = min(min_fc3_bcd, fc3_e)
+
                     self.clim_adj_yield[i_row, i_col] = original_yield[i_row, i_col] * self.fc3[i_row, i_col]
-                                    
 
     def getClimateAdjustedYield(self):
         """
